ea4883c8505f561fd644be405eed12873d380240
Alexis Narvaez Ruiz <zaizealexis@gmail.com>
2022-05-28 22:45:08 -0500

    Cambios en las texturas

--- js/Textures.js
+++ js/Textures.js
@@ -1,31 +1,19 @@
 /**
- * Constantes de ubicacion
- */
-const _ROCK = 0;
-const _ROCK_YELLOW = 1;
-const _WALL_GREEN = 2;
-/**
- * Constantes de texturas
- */
-const WALLS_TEXTURE = [
-  new THREE.TextureLoader().load("img/roca.jpg"),
-  new THREE.TextureLoader().load("img/roca-ama-2.jpg"),
-  new THREE.TextureLoader().load("img/pared-verde.jpg"),
-];
-/**
  * Funcion para poder proporcionar las texturas a cada uno de las paredes que las necesiten.
  * 
  * @param row Fila
  * @param column Columna
  * @param type Para saber si es una pared, venta o puerta
  * @param orientation La orientacion horizontal o vertical
+ * @param render Objeto encargado de renderizar las cosas.
  */
- const setTexture = (row, column, type, orientation) => {
+ const setTexture = (row, column, type, orientation,render,scene) => {
     let texture;
     let material = new THREE.MeshPhongMaterial({ color: 0xffffff });
     //Colocamos la textura de la piedra gris
     if ((row == 5 || row == 8) && orientation == "H") {
-      texture = new THREE.TextureLoader().load("img/roca.jpg");
+      //texture = new THREE.TextureLoader().load("img/roca.jpg");
+      texture = new THREE.ImageUtils.loadTexture('img/roca.jpg',{},function() { render.render(scene); });
       material = new THREE.MeshPhongMaterial({ map: texture });
     }
     //Colocamos la textura de la roca amarilla
--- js/app.js
+++ js/app.js
@@ -1,84 +1,83 @@
 const main = () => {
-    /**
-     * Creamos una ESCENA
-     */
-    const scene = new THREE.Scene();
-    /**
-     * Creamos la camara
-     */
-    const camera = new THREE.PerspectiveCamera(
-      75,
-      window.innerWidth / window.innerHeight,
-      0.1,
-      1000
-    );
-    camera.position.y = 10;
-    camera.lookAt(scene.position);
-    
-    /**
-     * Objeto encargado de pintar en la ESCENA
-     */
-    const renderer = new THREE.WebGLRenderer({ alpha: true });
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.shadowMap.enable = true;
-    document.body.appendChild(renderer.domElement);
-  
-    /**
-     * Objeto encargfado de mover la camara
-     */
-    const controls = new THREE.FirstPersonControls(camera, renderer.domElement);
-    controls.lookSpeed = 0.05;
+  /**
+   * Creamos una ESCENA
+   */
+  const scene = new THREE.Scene();
+  /**
+   * Creamos la camara
+   */
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  camera.position.y = 10;
+  camera.lookAt(scene.position);
+
+  /**
+   * Objeto encargado de pintar en la ESCENA
+   */
+  const renderer = new THREE.WebGLRenderer({ alpha: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.shadowMap.enable = true;
+  document.body.appendChild(renderer.domElement);
+
+  /**
+   * Objeto encargfado de mover la camara
+   */
+  const controls = new THREE.FirstPersonControls(camera, renderer.domElement);
+  controls.lookSpeed = 0.05;
   controls.movementSpeed = 50;
   controls.lookVertical = true;
   controls.mouseDragOn = true;
   controls.heightMin = 4;
   controls.heightMax = 5;
-    /**
-     * Cargamos las peredes.
-     * 
-     */
-    LoadWalls(scene);
+  /**
+   * Cargamos las peredes.
+   *
+   */
+  LoadWalls(scene,renderer);
 
-    /**
-     * Piso
-     */
-    const floor = new THREE.GridHelper(100, 100);
-    scene.add(floor);
-    /**
-     * Piso
-     */
-    const geometry = new THREE.PlaneGeometry(100, 100, 10);
-    const material = new THREE.MeshPhongMaterial({ color: 0xffffff });
-    const floor2 = new THREE.Mesh(geometry, material);
-    floor2.receiveShadow = true;
-    floor2.castShadow = true;
-    floor2.rotation.x = -Math.PI / 2;
-    scene.add(floor2);
-  
-    scene.background = new THREE.Color(0xffffff);
-  
-    /**
-     * Ejes
-     */
-    const axisHelper = new THREE.AxesHelper(100);
-    scene.add(axisHelper);
-  
-    //Luces
-    scene.add(new THREE.AmbientLight(0xffffff, 0.9));
-  
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.9);
-    directionalLight.position.x += 20;
-    directionalLight.position.y += 20;
-    directionalLight.position.z += 20;
-    directionalLight.castShadow = true;
-    scene.add(directionalLight);
-  
-    const animate = () => {
-      controls.update(0.01);
-      renderer.render(scene, camera);
-      requestAnimationFrame(animate);
-    };
-    animate();
+  /**
+   * Piso
+   */
+  const floor = new THREE.GridHelper(100, 100);
+  scene.add(floor);
+  /**
+   * Piso
+   */
+  const geometry = new THREE.PlaneGeometry(100, 100, 10);
+  const material = new THREE.MeshPhongMaterial({ color: 0xffffff });
+  const floor2 = new THREE.Mesh(geometry, material);
+  floor2.receiveShadow = true;
+  floor2.castShadow = true;
+  floor2.rotation.x = -Math.PI / 2;
+  scene.add(floor2);
+
+  scene.background = new THREE.Color(0xffffff);
+
+  /**
+   * Ejes
+   */
+  const axisHelper = new THREE.AxesHelper(100);
+  scene.add(axisHelper);
+
+  //Luces
+  scene.add(new THREE.AmbientLight(0xffffff, 0.9));
+
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 0.9);
+  directionalLight.position.x += 20;
+  directionalLight.position.y += 20;
+  directionalLight.position.z += 20;
+  directionalLight.castShadow = true;
+  scene.add(directionalLight);
+
+  const animate = () => {
+    controls.update(0.01);
+    renderer.render(scene, camera);
+    requestAnimationFrame(animate);
   };
-  document.addEventListener("DOMContentLoaded", main);
-  
+  animate();
+};
+document.addEventListener("DOMContentLoaded", main);
