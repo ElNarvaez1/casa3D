21c4ab306f851e5b17c28e68102fc2c7a714c1f5
Alexis Narvaez Ruiz <zaizealexis@gmail.com>
2022-05-29 00:31:20 -0500

    cambios

--- js/LoadWalls.js
+++ js/LoadWalls.js
@@ -223,4 +223,143 @@ const LoadWalls = (scene) =>{
           }
         }
       }
+
+
+
+      /**
+       * 
+       *Creamos las paredes de relleno 
+       */
+      for (let row = 0; row < WALLS_EXTER.length; row++) {
+        for (let column = 0; column < WALLS_EXTER[row].length; column++) {
+          switch (WALLS_EXTER[row][column]) {
+            case 1:
+              //Paredes Verticales.
+              //Pared
+              /**
+               * Necesitamos saber si es una pared es horizontal o vertical
+               */
+               if (column >= 0 && column != WALLS_EXTER[row].length - 1) {
+                //Horizontales
+                if (WALLS_EXTER[row][column + 1] != 0 || (column == 6 && row == 8)) {
+                  let width = 1;
+                  let height = 5;
+                  let prof = 0.2;
+                  let x = column == 0 ? width / 2 : column - width / 2;
+                  let z = row - prof * 1.5;
+    
+                  const geometry = new THREE.BoxGeometry(width, height, prof);
+                  const material = new THREE.MeshPhongMaterial({ color: 0xffffff });
+                  const cube = new THREE.Mesh(geometry, material);
+                  cube.position.x = x;
+                  cube.position.z = z;
+                  cube.position.y = 2.5;
+                  cube.castShadow = true;
+                  cube.receiveShadow = true;
+                  scene.add(cube);
+                }
+              }
+              if (row > 0 && row != WALLS_EXTER[row].length) {
+                //Verticales
+                if (WALLS_EXTER[row - 1][column] != 0) {
+                  let width = 0.2;
+                  let height = 5;
+                  let prof = 1;
+                  let x = column == 0 ? -width/2: column - prof;
+                  let z = row - prof / 2;
+    
+                  const material = new THREE.MeshPhongMaterial({ color: 0xffffff });
+                  const geometry = new THREE.BoxGeometry(width, height, prof);
+                  const cube = new THREE.Mesh(geometry, material);
+                  cube.position.x = x;
+                  cube.position.z = z;
+                  cube.position.y = 2.5;
+                  cube.castShadow = true;
+                  cube.receiveShadow = true;
+                  scene.add(cube);
+                }
+              }
+              break;
+              case 2:
+                //ventana
+              /**
+               * Necesitamos saber si es una ventana en  vertical o horizontal
+               */
+              if (column >= 0 && column != WALLS_EXTER[row].length - 1) {
+                //Horizontales
+                if (WALLS_EXTER[row][column + 1] != 0 || (column == 6 && row == 8)) {
+                  // seccion de la ventana de arriba
+                  let width = 1;
+                  let height = 1;
+                  let prof = 0.2;
+                  let x = column == 0 ? width / 2 : column - width / 2;
+                  let z = row - prof*1.5;
+    
+                  const geometry = new THREE.BoxGeometry(width, height, prof);
+                  const material = new THREE.MeshPhongMaterial({ color: 0xffffff });
+                  const cube = new THREE.Mesh(geometry, material);
+                  cube.position.x = x;
+                  cube.position.z = z;
+                  cube.position.y = 5 - height / 2;
+                  cube.castShadow = true;
+                  cube.receiveShadow = true;
+                  scene.add(cube);
+    
+                  //seccion de la ventana de abajo
+                  const geometryBottom = new THREE.BoxGeometry(width, height, prof);
+                  const materialBottom = new THREE.MeshPhongMaterial({ color: 0xffffff });
+                  const cubeBottom = new THREE.Mesh(geometryBottom, materialBottom);
+                  cubeBottom.position.x = x;
+                  cubeBottom.position.z = z;
+                  cubeBottom.position.y = height / 2;
+                  cubeBottom.castShadow = true;
+                  cubeBottom.receiveShadow = true;
+                  scene.add(cubeBottom);
+    
+                }
+              }
+              if (row > 0 && row != WALLS_EXTER[row].length) {
+                //Verticales
+                if (WALLS_EXTER[row - 1][column] != 0) {
+                  // seccion de la ventana de arriba
+                  let width = 0.2;
+                  let height = 1;
+                  let prof = 1;
+                  let x = column == 0 ? -width / 2 : column - prof;
+                  let z = row - prof / 2;
+    
+                  const geometry = new THREE.BoxGeometry(width, height, prof);
+                  const material = new THREE.MeshPhongMaterial({
+                    color: 0xffffff,
+                  });
+                  const cube = new THREE.Mesh(geometry, material);
+                  cube.position.x = x;
+                  cube.position.z = z;
+                  cube.position.y = 5 - height / 2;
+                  cube.castShadow = true;
+                  cube.receiveShadow = true;
+                  scene.add(cube);
+    
+                  //seccion de la ventana de abajo
+                  const geometryBottom = new THREE.BoxGeometry(width, height, prof);
+                  const materialBottom = new THREE.MeshPhongMaterial({
+                    color: 0xffffff,
+                  });
+                  const cubeBottom = new THREE.Mesh(geometryBottom, materialBottom);
+                  cubeBottom.position.x = x;
+                  cubeBottom.position.z = z;
+                  cubeBottom.position.y = height / 2;
+                  cubeBottom.castShadow = true;
+                  cubeBottom.receiveShadow = true;
+                  scene.add(cubeBottom);
+    
+                }
+              }
+                break;
+          
+            default:
+              break;
+          }
+        }
+      }
 }
